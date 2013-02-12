bd38181471009933f30b2c6ab27eebb6b65b0fd7
laiso <laiso@lai.so>
2013-02-13 04:15:07 +0900

    見た目整えた

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -33,10 +33,12 @@ define(['config', 'garlic', 'jquery.multi-select', 'jquery.quicksearch'], functi
 
 
   // ストア情報
+  var recentSelected = localStorage.getItem('selectedStore').split(",");
   $.each(config.sources, function(i, source){
     var $option = $("<option>")
       .val(source.id)
       .text(source.name);
+    if($.inArray(source.id, recentSelected) > -1) $option.attr("selected", true);
     $("#stores").append($option);
   });
 
@@ -44,22 +46,30 @@ define(['config', 'garlic', 'jquery.multi-select', 'jquery.quicksearch'], functi
   // ストア取得
   var getSelectedStore = function(){
     var result = [];
+    var saveItems = [];
     $.each($("#stores").serializeArray(), function(i, store){
       var f = function(s){
         return function(){
           $.each(config.sources, function(i, source){
-            if(s.value === source.id) result.push(source);
+            if(s.value === source.id){
+              result.push(source);
+              saveItems.push(s.value);
+            }
           });
         }
       };
       f(store)();
     });
+
+    localStorage.setItem('selectedStore', saveItems);
+
     return result;
   };
 
   // multiselect + quicksearch
   $("#stores").multiSelect({
     selectableHeader:"<input type='text' id='search' autocomplete='off' placeholder=\"絞り込み...\">",
+    selectionHeader: "<label class=\"selectionLabel\">選択中(再度クリックして解除)</label>",
     afterDeselect: function(value){
       $("form#"+value[0]).remove();
     }
